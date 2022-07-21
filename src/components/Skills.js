32e864073ca5f8643a656a27574fb3ac2a0910b8
= <=>
2022-07-21 13:58:09 +1000

    adjust

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -47,19 +47,19 @@ export const Skills = () => {
               >
                 <div className="item">
                   <img src={meter1} alt="Image" />
-                  <h5>Web Development</h5>
+                  <h5>React</h5>
                 </div>
                 <div className="item">
                   <img src={meter2} alt="Image" />
-                  <h5>Brand Identity</h5>
+                  <h5>NodeJS</h5>
                 </div>
                 <div className="item">
                   <img src={meter3} alt="Image" />
-                  <h5>Logo Design</h5>
+                  <h5>HTML-CSS</h5>
                 </div>
                 <div className="item">
                   <img src={meter1} alt="Image" />
-                  <h5>Dick Head</h5>
+                  <h5>JavaScript</h5>
                 </div>
               </Carousel>
             </div>
